8a2e89d9f1be9313e264da610a08308bdf5e9277
Sonadi <144614600+Sonadi@users.noreply.github.com>
2025-03-26 11:13:35 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ProtectedRoute from './routes/ProtectedRoute';
+//import ProtectedRoute from './routes/ProtectedRoute';
 import Login from './features/auth/Login';
 import Dashboard from './pages/Dashboard';
 
 
 const App = () => {
   return (
-    <BrowserRouter>
+   
       <Routes>
         <Route path="/login" element={<Login />} />
-        <ProtectedRoute path="/admin-dashboard" element={<Dashboard />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/admin-dashboard" element={<Dashboard />} />
+       
       </Routes>
-    </BrowserRouter>
+   
   );
 };
 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,15 @@
-// src/pages/Dashboard.jsx
+
 import React from 'react';
-import ProtectedRoute from '../routes/ProtectedRoute';
+
 
 const Dashboard = () => {
   return (
-    <ProtectedRoute>
+   
       <div>
         <h1>Admin Dashboard</h1>
         {/* Admin dashboard content */}
       </div>
-    </ProtectedRoute>
+   
   );
 };
 
