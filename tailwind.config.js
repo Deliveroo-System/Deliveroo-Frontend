b595f477d4322e1c663dae82ab2d48280c2daa99
vidura-12 <viduranirmal195@gmail.com>
2025-03-27 09:13:26 +0530

    Implement register function

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
-// tailwind.config.js
-module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {
-        colors: {
-          primary: '#3E3F5B',
-          secondary: '#8AB2A6',
-          accent: '#ACD3A8',
-        }
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3E3F5B',
+        secondary: '#8AB2A6',
+        accent: '#ACD3A8',
       }
-    },
-    plugins: [],
-  }
+    }
+  },
+  plugins: [],
+}
